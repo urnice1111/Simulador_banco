52d002f0f1c71e104b7952d801ee04cfbb2fab12
Nestor1306 <a01754717@tec.mx>
2025-06-04 20:45:12 -0600

    BankAccount.h

--- BankAccount.h
+++ BankAccount.h
@@ -1,15 +1,29 @@
+//--------------------------------------------------
+//  Proyecto simulador de Sistema Bancario
+//  15 de Junio del 2025
+//  Autores:
+//      A01754717 Nestor Daniel Huidobro Aguilar
+//      A01753803 Emiliano Garcia Ramos 
+//---------------------------------------------------
+
+#ifndef BANK_ACCOUNT_H
+#define BANK_ACCOUNT_H
+
+
+
 #pragma once
 
 #include <string>
+#include <iostream>
 
-
-
+// Clase base:BankAccount
 class BankAccount {
 public:
     virtual void deposit(double amount);
     virtual void withdraw(double amount); //Virtual class to be overrided on dependences
     virtual std::string to_string() const;
 
+    virtual~BankAccount() = default; // virtual destroyer 
 private:
     double _balance;
     std::string _account_number;
@@ -17,3 +31,5 @@ private:
 };
 
 std::ostream& operator<<(std::ostream& os, const BankAccount& BankAccount);
+
+#endif // BANK_ACCOUNT_H
