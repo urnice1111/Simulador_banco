07ba9d0b57c3755557f378ec26e857d013e5c786
urnice1111 <emilianogarram2910@gmail.com>
2025-06-09 15:48:12 -0600

    All files created

---
+++ SavingsAccount.h
@@ -0,0 +1,18 @@
+#include "BankAccount.h"
+#include <iostream>
+#pragma once
+
+class SavingsAccount: public BankAccount{
+
+    private:
+
+
+
+    public:
+
+    void deposit(double amount) override;
+
+};
+
+
+
