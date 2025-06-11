415a05598180d0a7adaa9061714b5fb710ab9984
Nestor1306 <a01754717@tec.mx>
2025-06-11 12:34:50 -0600

    Update CheckingAccount.cpp

--- CheckingAccount.cpp
+++ CheckingAccount.cpp
@@ -5,3 +5,25 @@
 //      A01754717 Nestor Daniel Huidobro Aguilar
 //      A01753803 Emiliano Garcia Ramos 
 //---------------------------------------------------
+
+#include "CheckingAccount.h"
+#include <stdexcept>
+
+void CheckingAccount::deposit(double amount){
+  if (amount <= 0) {
+    throw std::invalid_argument("El monto del deposito debe de ser mayor que cero");
+  }
+  //Depositar el monto
+  _balance += amount;
+}
+
+void CheckingAccount::withdraw(double amount){
+  if (amount <=0){
+throw std::invalid_argument("El monto del retiro debe de ser mayor que cero");
+  }
+if (amount > _balance + _overdraft_limit){
+
+throw std::logic_error("se supero el limite de sobregiro");
+}
+_balance -= amount;
+}
