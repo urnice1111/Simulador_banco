feeebfafa7aa9a993c0c2a8af4138238b8036504
Nestor1306 <a01754717@tec.mx>
2025-06-11 12:24:34 -0600

    Update CheckingAccount.h

--- CheckingAccount.h
+++ CheckingAccount.h
@@ -5,3 +5,25 @@
 //      A01754717 Nestor Daniel Huidobro Aguilar
 //      A01753803 Emiliano Garcia Ramos 
 //---------------------------------------------------
+
+
+#pragma once
+#include "BankAccount.h"
+#include <iostream>
+
+// clase derivada :chekingAccount
+class CheckingAccount : public BankAccount {
+public:
+   // Limite de sobregiro
+double _overdraft_limit;
+
+explicit checkingAccount(
+const std::string& account_holder_name,
+const std::string& account_number,
+double overdraft_limit)
+:BankAccount(balance, account_number,account_holder_name), _overdraft_limit(overdraft_limit){}
+
+void deposit(double amount) override;
+
+void withdraw(double amount) override;
+};
